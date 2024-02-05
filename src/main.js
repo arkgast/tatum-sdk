e4400d8e83cd17bdd69fb76caac6d3fc723128f6
Arnold Gandarillas Castillo <arkgast@gmail.com>
2024-02-05 17:58:00 -0400

    feat: generate private key

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
-const { TatumSDK, Network, ApiVersion } = require("@tatumio/tatum");
+const { config } = require("dotenv");
+const { TatumSDK, Network } = require("@tatumio/tatum");
+const { EvmWalletProvider } = require("@tatumio/evm-wallet-provider");
 
-async function initialize() {
+config();
+
+const MNEMONIC = process.env.MNEMONIC;
+
+const initializeTatumSdk = async () => {
   return await TatumSDK.init({
-    network: Network.ETHEREUM,
-    version: ApiVersion.V4,
+    network: Network.POLYGON,
+    configureWalletProviders: [EvmWalletProvider],
   });
+};
+
+async function generatePrivateKey() {
+  const tatumSdk = await initializeTatumSdk();
+
+  const privateKey = await tatumSdk.walletProvider
+    .use(EvmWalletProvider)
+    .generatePrivateKeyFromMnemonic(MNEMONIC, 1);
+
+  console.log({ privateKey });
 }
 
-async function getEstimateFee() {
-  const tatum = await initialize();
-
-  return tatum.fee.getCurrentFee();
-}
-
-getEstimateFee();
+generatePrivateKey().then(() => process.exit(0));
