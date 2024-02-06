b304de38c3e52b191bb3d3e2a1b1276028e5a519
Arnold Gandarillas Castillo <arkgast@gmail.com>
2024-02-06 11:39:43 -0400

    feat: sign and broadcast transaction

--- src/main.js
+++ src/main.js
@@ -6,11 +6,17 @@ config();
 
 const MNEMONIC = process.env.MNEMONIC;
 const XPUB = process.env.XPUB;
+const TATUM_API_KEY = process.env.TATUM_API_KEY;
+
+const TO_ADDRESS = "0x5014E84d22D69e1607cA5eE6F3DB57E2Cb442843";
 
 const initializeTatumSdk = async () => {
   return await TatumSDK.init({
     network: Network.ETHEREUM_SEPOLIA,
     configureWalletProviders: [EvmWalletProvider],
+    apiKey: {
+      v4: TATUM_API_KEY,
+    },
   });
 };
 
@@ -22,6 +28,8 @@ async function generatePrivateKey() {
     .generatePrivateKeyFromMnemonic(MNEMONIC, 1);
 
   console.log({ privateKey });
+
+  return privateKey;
 }
 
 async function generateAddress() {
@@ -38,14 +46,35 @@ async function generateAddress() {
   console.log({ addressFromMnemonic, addressFromXpub });
 }
 
+async function signAndBroadcastTransaction() {
+  const tatumSdk = await initializeTatumSdk();
+
+  const privateKey = await generatePrivateKey();
+
+  const payload = {
+    privateKey,
+    to: TO_ADDRESS,
+    value: "0.01",
+  };
+
+  const txHash = await tatumSdk.walletProvider
+    .use(EvmWalletProvider)
+    .signAndBroadcast(payload);
+
+  console.log({ txHash });
+}
+
 async function main() {
   await generatePrivateKey();
 
   await generateAddress();
+
+  await signAndBroadcastTransaction();
 }
 
 main()
   .then(() => process.exit(0))
   .catch((e) => {
     console.log(e);
+    process.exit(1);
   });
